2b49ff0b8f11884f95fd4bf32466e212cabe8312
pipetapasco <pipetapasco21@gmail.com>
2026-01-05 00:48:23 -0500

    refactor: Remove `AllowedHostsOriginValidator` from WebSocket routing configuration.

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -20,7 +20,6 @@ django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack  # noqa: E402
 from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
-from channels.security.websocket import AllowedHostsOriginValidator  # noqa: E402
 
 from apps.chat.routing import websocket_urlpatterns  # noqa: E402
 
@@ -29,6 +28,6 @@ application = ProtocolTypeRouter(
         # HTTP requests -> Django REST API
         'http': django_asgi_app,
         # WebSocket connections -> Django Channels
-        'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
+        'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
